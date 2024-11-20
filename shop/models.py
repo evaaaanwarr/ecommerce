d4286a502cc9e20cb4e111cb515c6a010347951a
evaaaanwarr <evaaanwarrr@gmail.com>
2024-11-20 14:34:56 +0800

    Create

---
+++ shop/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.utils.text import slugify
+
+class Kategori(models.Model):
+    name = models.CharField(max_length=10, unique=True, verbose_name="Nama Kategori")
+    slug = models.SlugField(max_length=100, unique=True, blank=True)
+    description = models.TextField(verbose_name="Deskripsi", blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Dibuat pada")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Diperbarui pada")
+    price = models.DecimalField(blank=False)
+
+    class Meta:
+        verbose_name = "Kategori"
+        verbose_name_plural = "Kategori"
+        ordering = ['name']
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
